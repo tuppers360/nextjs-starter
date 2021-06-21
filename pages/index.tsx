7947930a87c5e093868ef97d8f3afe5988d19ee8
tuppers360 <tuppers360@live.co.uk>
2021-06-21 19:44:09 +0100

    upgrade packages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <div className="flex items-center justify-center h-screen">
-      <h1 className="text-9xl font-bold p-16 text-center leading-snug">
-        Welcome to Next.js! with Typescript and Tailwind
+      <h1 className="p-16 font-bold leading-snug text-center text-9xl">
+        Welcome to Next.js! (11) with Typescript and Tailwind (2.2)
       </h1>
     </div>
   );
